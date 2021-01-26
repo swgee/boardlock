67287aae8462bb6dfdaf2d1be2e11b8187e5796c
swgee <acoolio404@protonmail.com>
2021-01-26 08:56:31 -0600

    Signup page complete

--- password-manager/static/js/signup-page.js
+++ password-manager/static/js/signup-page.js
@@ -8,12 +8,14 @@ let lengthmsg = document.getElementById('lengthCheck');
 let specialmsg = document.getElementById('specialCheck');
 let numbermsg = document.getElementById('numberCheck');
 let capitalmsg = document.getElementById('capitalCheck');
-lengthmsg.classList.add('incomplete'); specialmsg.classList.add('incomplete'); numbermsg.classList.add('incomplete'); capitalmsg.classList.add('incomplete');
-messages = [specialmsg,lengthmsg,capitalmsg,numbermsg]
+let matchmsg = document.getElementById('match');
+lengthmsg.classList.add('incomplete'); specialmsg.classList.add('incomplete'); numbermsg.classList.add('incomplete'); capitalmsg.classList.add('incomplete'); matchmsg.classList.add('incomplete');
+messages = [specialmsg,lengthmsg,capitalmsg,numbermsg,matchmsg]
 
-function validation() {
+function validation() { // client side only, server side also implemented in case of tampering
     let psw = document.getElementById('psw').value;
     let confirm_psw = document.getElementById('confirm-psw').value;
+    let username = document.getElementById('username').value;
     if (psw.length >= 12) {lengthmsg.classList.replace("incomplete","complete");}
     else {lengthmsg.classList.replace('complete','incomplete');}
     for (let i = 0; i < uppercase.length; i++) {
@@ -25,10 +27,12 @@ function validation() {
     for (let i = 0; i < special.length; i++) {
         if (psw.includes(special[i])) {specialmsg.classList.replace("incomplete","complete"); break;}
         else {specialmsg.classList.replace('complete','incomplete');}}
+    if (psw === confirm_psw) {matchmsg.classList.replace('incomplete','complete')}
+    else {matchmsg.classList.replace('complete','incomplete')}
     for (let i = 0; i < messages.length; i++) {
-        if (messages[i].classList.contains('complete') && confirm_psw.length == psw.length) {
+        if (messages[i].classList.contains('complete') && username.length <= 64 && psw.length <= 128) {
             submitButton.disabled = false;
-        } else {submitButton.disabled = true;}
+        } else {submitButton.disabled = true; break;}
     }
 }
 
