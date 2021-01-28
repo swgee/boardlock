d60bd72f29cb7885a05fb569a5d2c7a593f3c282
swgee <acoolio404@protonmail.com>
2021-01-27 18:09:41 -0600

    New account encryption started

--- password-manager/authentication/create_account.py
+++ password-manager/authentication/create_account.py
@@ -1,7 +1,11 @@
 import boto3
 from boto3.dynamodb.conditions import Key
 from datetime import datetime
+import bcrypt
+from cryptography.fernet import Fernet
+import csv
 
+top_row = ["Title", "Password", "URL", "Notes"]
 
 dynamodb = boto3.resource('dynamodb')
 table = dynamodb.Table('boardlock-data')
@@ -10,13 +14,29 @@ def store_credentials(username, password):
     response = table.query(
         KeyConditionExpression=Key('username').eq(username)
     )
-    if len(response['Items']) == 0:
+    if len(response['Items']) > 0:
+        return False
+    else:
+        password = bytes(password)
+        salt = bcrypt.gensalt()
+        auth_key = bcrypt.hashpw(password, salt)
+        kek = bcrypt.kdf(password, salt, 32, 100)
+        data_key = Fernet.generate_key()
+        with open('quotes.csv', 'w', newline='') as new_csv:
+            writer = csv.writer(new_csv, quoting=csv.QUOTE_NONNUMERIC, delimiter=';')
+            writer.writerows(top_row)
+        data_encrypt = Fernet(data_key)
+        new_csv = bytes(new_csv)
+        encrypted_csv = data_encrypt.encrypt(new_csv)
+        key_encrypt = Fernet(kek)
+        encrypted_data_key = key_encrypt.encrypt(data_key)
         table.put_item(
             Item={
                 'username': username,
-                'password': password,
+                'auth_key': auth_key,
+                'salt': salt,
+                'csv': encrypted_csv,
+                'data_key': encrypted_data_key,
                 'time_created': datetime.now().strftime("%m/%d/%Y %H:%M:%S"),
             }
         )
-    else:
-        return False
